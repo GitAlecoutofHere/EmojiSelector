ce392a1f7fa634437490b3ffaf7dfc2371dd508c
AlecOverman <aover003@ucr.ecu>
2023-09-27 20:19:04 -0700

    Initial Commit

---
+++ RandomPhoto/RandomPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomPhotoApp.swift
+//  RandomPhoto
+//
+//  Created by Alec Overman on 9/27/23.
+//
+
+import SwiftUI
+
+@main
+struct RandomPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
